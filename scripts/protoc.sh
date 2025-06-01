7ebc8460f4f54978d6d9db3b00b5fd82a8050916
niuhuan <niuhuancn@outlook.com>
2025-06-01 23:29:34 +0800

    ff_pro

--- scripts/protoc.sh
+++ scripts/protoc.sh
@@ -1,7 +1,7 @@
 
-# go install github.com/golang/protobuf/protoc-gen-go@latest
+# go install go install github.com/golang/protobuf/protoc-gen-go@1.5.2
 
-# dart pub global activate protoc_plugin
+# dart pub global activate protoc_plugin@21.1.2
 # export PATH="$PATH":"$HOME/.pub-cache/bin"
 
 #protoc --go_out=go/ protos/*.proto
